8a489cba65ccbe1fa41271a0f25d9b1437af4a20
minhtuan1611 <tuan.mn51@gmail.com>
2024-04-11 09:36:13 +0300

    add reacstrap and bootstrap

--- portfolio-minhtuan/src/app/layout.js
+++ portfolio-minhtuan/src/app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from 'next/font/google'
-import Script from 'next/script'
-
-const inter = Inter({ subsets: ['latin'] })
+import BootstrapClient from '@/components/BootstrapClient.js'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export const metadata = {
   title: "Mtuan's Portfolio",
@@ -11,8 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></Script>
-      <body className={inter.className}>{children}</body>
+      <body>
+        {children}
+        <BootstrapClient />
+      </body>
     </html>
   )
 }
--- portfolio-minhtuan/src/app/page.js
+++ portfolio-minhtuan/src/app/page.js
@@ -1,9 +1,15 @@
+'use client'
 import BaseLayout from './components/layouts/BaseLayout'
+import { Container, Button } from 'reactstrap'
 
 export default function Home() {
   return (
     <BaseLayout>
-      <h1>I am index page</h1>
+      <Container>
+        <h2>I'm an index page</h2>
+
+        <Button color="primary">Click Me</Button>
+      </Container>
     </BaseLayout>
   )
 }
