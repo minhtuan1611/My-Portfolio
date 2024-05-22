57d525475baa47aad614785eb9304cc817a4eb6f
minhtuan1611 <tuan.mn51@gmail.com>
2024-05-23 02:11:33 +0300

    delete old verstion

--- mtuan-portfolio-api/controllers/portfolios.js
+++
@@ -1,3 +0,0 @@
-exports.getPortfolios = (req, res) => {
-  return res.json({ data: [10, 20, 30, 40] })
-}
--- mtuan-portfolio-api/routes/portfolios.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getPortfolios } = require('../controllers/portfolios')
-
-router.get('', getPortfolios)
-
-module.exports = router
